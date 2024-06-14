e0c363a6e867357866b421c576b9a7b084ff6a63
Ramzy Kermad <kermad.r@sncf.fr>
2024-06-14 17:56:54 +0200

    Implemented parcelable to todomodel

--- app/src/main/java/com/example/myfirstapp/model/TodoModel.kt
+++ app/src/main/java/com/example/myfirstapp/model/TodoModel.kt
@@ -1,3 +1,35 @@
 package com.example.myfirstapp.model
 
-data class TodoModel(val title: String, val date: String, val isChecked: Boolean)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class TodoModel(val title: String?, val description: String?, val date: String?, val isChecked: Boolean): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readByte() != 0.toByte()
+    ) {
+    }
+
+    override fun describeContents(): Int {
+        return hashCode()
+    }
+
+    override fun writeToParcel(parcel: Parcel, p1: Int) {
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(date)
+        parcel.writeValue(isChecked)
+    }
+
+    companion object CREATOR : Parcelable.Creator<TodoModel> {
+        override fun createFromParcel(parcel: Parcel): TodoModel {
+            return TodoModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<TodoModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
